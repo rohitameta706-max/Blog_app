37d3e993b4b2e43702d1217799ff402b2e88e7dc
rohitameta706-max <rohitameta666@gmail.com>
2026-08-12 18:11:23 +0530

    Revert Cloudinary settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_ROOT= BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = "static/"
 
